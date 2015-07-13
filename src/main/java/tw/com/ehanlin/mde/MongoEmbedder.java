2d6ef764e88cc000fcce7f80950a14643244cab4
hotdog <hotdog929@gmail.com>
2015-07-13 18:04:25 +0800

    加入 parallel 功能

--- src/main/java/tw/com/ehanlin/mde/MongoEmbedder.java
+++ src/main/java/tw/com/ehanlin/mde/MongoEmbedder.java
@@ -36,16 +36,32 @@ public class MongoEmbedder {
         return embed(null, dslStr);
     }
 
+    public Object embed(String dslStr, Boolean parallel) {
+        return embed(null, dslStr, parallel);
+    }
+
     public Object embed(Dsl dsl) {
         return embed(null, dsl);
+    }
+
+    public Object embed(Dsl dsl, Boolean parallel) {
+        return embed(null, dsl, parallel);
     }
 
     public Object embed(Object resource, String dslStr) {
         return embed(resource, dslParser.parse(dslStr));
     }
 
+    public Object embed(Object resource, String dslStr, Boolean parallel) {
+        return embed(resource, dslParser.parse(dslStr), parallel);
+    }
+
     public Object embed(Object resource, Dsl dsl) {
         return dsl.execute(new DataStack(null, resource), dbMap, new ConcurrentHashMap(), false);
+    }
+
+    public Object embed(Object resource, Dsl dsl, Boolean parallel) {
+        return dsl.execute(new DataStack(null, resource), dbMap, new ConcurrentHashMap(), parallel);
     }
 
     public void register(String key, DB db){
